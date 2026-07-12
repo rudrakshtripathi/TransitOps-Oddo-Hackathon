431cdeaab6dab438dc45ab2422e0a167ec20e280
Harsh Narayan Jha <harshnj@proton.me>
2026-07-12 13:39:51 +0530

    auth: login and signup

--- src/app.d.ts
+++ src/app.d.ts
@@ -1,21 +1,21 @@
 // See https://svelte.dev/docs/kit/types#app.d.ts
 
-import type { User } from "$lib/types";
-import type { Session, SupabaseClient } from "@supabase/supabase-js";
+import type { Session, SupabaseClient, User } from "@supabase/supabase-js"
 
 // for information about these interfaces
 declare global {
-	namespace App {
-		// interface Error {}
+  namespace App {
+    // interface Error {}
     interface Locals {
       supabase: SupabaseClient
       safeGetSession: () => Promise<{ session: Session | null; user: User | null }>
       session: Session | null
       user: User | null
-		// interface PageData {}
-		// interface PageState {}
-		// interface Platform {}
-	}
+    }
+    // interface PageData {}
+    // interface PageState {}
+    // interface Platform {}
+  }
 }
 
-export {};
+export {}
