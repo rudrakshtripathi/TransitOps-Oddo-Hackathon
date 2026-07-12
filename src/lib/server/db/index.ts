e007f3470ba733ddc1d786976eff9dce076e47c2
deoxyssh <294086870+deoxyssh@users.noreply.github.com>
2026-07-12 13:39:51 +0530

    auth: login and signup

---
+++ src/lib/server/db/index.ts
@@ -0,0 +1,11 @@
+import { DATABASE_URL } from "$env/static/private"
+
+import * as schema from "$lib/db/schema"
+
+import { drizzle } from "drizzle-orm/postgres-js"
+import postgres from "postgres"
+
+if (!DATABASE_URL) throw new Error("DATABASE_URL is not set")
+
+const client = postgres(DATABASE_URL, { prepare: false })
+export const db = drizzle(client, { schema })
