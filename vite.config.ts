f060c269bd4092897fea4853ecb468e8dfeb6b24
Harsh Narayan Jha <harshnj@proton.me>
2026-07-12 14:25:38 +0530

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,6 @@
-import tailwindcss from "@tailwindcss/vite"
+import adapter from "@sveltejs/adapter-vercel"
 import { sveltekit } from "@sveltejs/kit/vite"
-import adapter from "@sveltejs/adapter-static"
+import tailwindcss from "@tailwindcss/vite"
 import { defineConfig } from "vite"
 
 export default defineConfig({
