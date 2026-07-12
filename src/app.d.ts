22295004960b7a1ee08bb9d3b2cfafed6441348e
Harsh Narayan Jha <harshnj@proton.me>
2026-07-12 11:59:46 +0530

    init shadcn + supabase auth

--- src/app.d.ts
+++ src/app.d.ts
@@ -1,9 +1,17 @@
 // See https://svelte.dev/docs/kit/types#app.d.ts
+
+import type { User } from "$lib/types";
+import type { Session, SupabaseClient } from "@supabase/supabase-js";
+
 // for information about these interfaces
 declare global {
 	namespace App {
 		// interface Error {}
-		// interface Locals {}
+    interface Locals {
+      supabase: SupabaseClient
+      safeGetSession: () => Promise<{ session: Session | null; user: User | null }>
+      session: Session | null
+      user: User | null
 		// interface PageData {}
 		// interface PageState {}
 		// interface Platform {}
--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,9 +1,40 @@
 <script lang="ts">
 	import './layout.css';
-	import favicon from '$lib/assets/favicon.svg';
 
-	let { children } = $props();
+	import { invalidate } from '$app/navigation';
+
+	import { Toaster } from '$lib/components/ui/sonner';
+
+	import favicon from '$lib/assets/favicon.svg';
+	import { SUPABASE_AUTH_TAG } from '$lib/constants';
+
+	import { ModeWatcher } from 'mode-watcher';
+	import { onMount } from 'svelte';
+
+	let { data, children } = $props();
+	let { session, supabase } = $derived(data);
+
+	onMount(() => {
+		const { data } = supabase.auth.onAuthStateChange((changeEvent, newSession) => {
+			console.log(`Auth State changed: ${changeEvent}`, newSession);
+
+			if (newSession?.expires_at != session?.expires_at) {
+				console.log('Invalidating current auth session');
+				invalidate(SUPABASE_AUTH_TAG);
+			}
+		});
+
+		return () => data.subscription.unsubscribe();
+	});
 </script>
 
-<svelte:head><link rel="icon" href={favicon} /></svelte:head>
-{@render children()}
+<svelte:head>
+	<link rel="icon" href={favicon} />
+</svelte:head>
+
+<!-- Root Layout -->
+<ModeWatcher />
+
+<Toaster richColors />
+
+{@render children?.()}
